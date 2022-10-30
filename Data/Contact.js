84e2d98fcbe4cf659ff6a88012e2043b2148d6f1
Saksham Bindal <saksham.bindal2004@gmail.com>
2022-10-30 23:53:13 +0530

    phone hide

--- Data/Contact.js
+++ Data/Contact.js
@@ -3,9 +3,9 @@ const contact_links = {
   instagram: "https://www.instagram.com/im_bbrocks.1908",
   github: "https://github.com/bin5181",
   linkedin: "https://www.linkedin.com/in/bh5181/",
-  phone: "+91 6378057878",
+  phone: "+91 63********",
   email: "bindal.bhanu19082002@gmail.com",
-  website: "https://www.imbbrocks.me",
+  website: "https://www.imbbrocks.co.in",
 };
 
 export default contact_links;
