1ac3c5fb4c7a34823c2ca8eb6098ed0d4fa38e34
Bhanu Prakash Bindal <93444297+bin5181@users.noreply.github.com>
2023-07-29 14:59:19 +0530

    Update About.js

--- Data/About.js
+++ Data/About.js
@@ -13,7 +13,7 @@ const about = {
   about: "Full Stack Developer, Software Developer, Graphic Designer, Competitive Programmer",
   bio: "Pursuing B. Tech at NIT Kurukshetra in Electronics & Communication Branch. 3rd Year Student.",
   longBio:
-    "I'm a developer with experience in building websites for individuals and organizations. I am expert in Python, CPP, MySQL, Design. I am a competitive Programmer. I have solved more than 500 questions of DSA on various coding platforms.",
+    "I'm a developer with experience in building websites for individuals and organizations. I am expert in Python, CPP, MySQL, Design. I am a competitive Programmer. I have solved more than 800 questions of DSA on various coding platforms.",
   birthday: "19 Aug 2002",
   city: "Hindaun, Rajasthan, India",
   age: year,
